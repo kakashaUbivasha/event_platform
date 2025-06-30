76ffc0f17aaac7efd42f09b2432dcba7f3e21c05
kakashaUbivasha <makspala777@gmail.com>
2025-06-30 16:46:22 +0500

    event migration

--- database/migrations/2025_06_30_113228_create_events_table.php
+++ database/migrations/2025_06_30_113228_create_events_table.php
@@ -12,7 +12,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('events', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->string('title');
+            $table->uuid('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->text('description');
+            $table->id('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->string('location')->nullable();
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->string('image')->nullable();
+            $table->boolean('is_private')->default(false);
             $table->timestamps();
         });
     }
