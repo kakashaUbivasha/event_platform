a104b81f3bc2512c7a60cc243fcbd6195b9d1fdf
kakashaUbivasha <makspala777@gmail.com>
2025-07-05 18:14:45 +0500

    Attachment model update

--- app/Models/Attachment.php
+++ app/Models/Attachment.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Attachment extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function events()
+    {
+        return $this->belongsTo(Event::class);
+    }
 }
