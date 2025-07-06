95267d44b234610a81cc6902457796f5b3000bc9
kakashaUbivasha <makspala777@gmail.com>
2025-07-06 21:22:18 +0500

    Comment model upodate

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function events()
+    {
+        return $this->belongsTo(Event::class);
+    }
+    public function likes()
+    {
+        return $this->morphMany(Like::class, 'likeable');
+    }
 }
