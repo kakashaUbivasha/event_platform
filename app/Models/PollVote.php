07be6b2dceb1aca3d8519cba0913491e95a0f7f3
kakashaUbivasha <makspala777@gmail.com>
2025-07-10 11:13:13 +0500

    poll vote model

--- app/Models/PollVote.php
+++ app/Models/PollVote.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use PhpOption\Option;
 
 class PollVote extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function poll()
+    {
+        return $this->belongsTo(Poll::class);
+    }
+    public function option()
+    {
+        return $this->belongsTo(Option::class);
+    }
 }
