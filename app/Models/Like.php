804fa77c4445ac6d6cd2fe8963c23a88bde29221
kakashaUbivasha <makspala777@gmail.com>
2025-07-10 11:09:02 +0500

    like model

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Like extends Model
 {
     use HasFactory;
+
+    protected $table = 'likes';
+    protected $guarded = [];
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function likeable()
+    {
+        return $this->morphTo();
+    }
 }
