deac83ad27b296535dc7f2841105b139ed263aea
kakashaUbivasha <makspala777@gmail.com>
2025-07-10 11:19:39 +0500

    login controller+request

---
+++ app/Http/Controllers/User/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        $data = $request->validated();
+
+        $user = User::where('email', $data['email'])->first();
+        if (!$user || !Hash::check($data['password'], $user->password)) {
+            return response(['message' => 'Invalid credentials'], 401);
+        }
+        $token = $user->createToken('auth_token')->plainTextToken;
+        if(!$token)
+        {
+            return response(['message' => 'Error create token'], 501);
+        }
+        return response(['user' => $user, 'token' => $token]);
+    }
+}
